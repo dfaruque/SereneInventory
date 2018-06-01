3d7a49fe18cf4a6c47b846c6c279e1257d050588
dFaruque <dfaruque@gmail.com>
2018-06-01 18:05:41 +0600

    added inventory migration

---
+++ SereneInventory/SereneInventory.Web/Migrations/DefaultDB/DefaultDB_20180601_130000_Inventory.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace SereneInventory.Migrations.DefaultDB
+{
+    [Migration(20180601130000)]
+    public class DefaultDB_20180601_130000_Inventory : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            this.CreateTableWithId64("Product", "Id", s => s
+                .WithColumn("Name").AsGuid().NotNullable()
+                .WithColumn("Description").AsString(50).NotNullable());
+
+            
+        }
+    }
+}
