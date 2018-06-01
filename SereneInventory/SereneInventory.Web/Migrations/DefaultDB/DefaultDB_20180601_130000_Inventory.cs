c1dd64657cfc610ab92afbe8a8f3585d40ff72ce
dFaruque <dfaruque@gmail.com>
2018-06-01 21:51:01 +0600

    inventory migration

--- SereneInventory/SereneInventory.Web/Migrations/DefaultDB/DefaultDB_20180601_130000_Inventory.cs
+++ SereneInventory/SereneInventory.Web/Migrations/DefaultDB/DefaultDB_20180601_130000_Inventory.cs
@@ -9,10 +9,35 @@ namespace SereneInventory.Migrations.DefaultDB
         {
             this.CreateTableWithId64("Product", "Id", s => s
                 .WithColumn("Name").AsString(100).NotNullable()
+                .WithColumn("ProductType").AsInt32().Nullable()
+                .WithColumn("ProductCategory").AsInt32().Nullable()
                 .WithColumn("Description").AsString(500).Nullable()
+                .WithColumn("Images").AsString(500).Nullable()
                 .CommonFields());
 
-            
+            this.CreateTableWithId64("Party", "Id", s => s
+                .WithColumn("PartyType").AsInt32().NotNullable()
+                .WithColumn("Name").AsString(100).NotNullable()
+                .WithColumn("Address").AsString(500).Nullable()
+                .WithColumn("Contact").AsString(500).Nullable()
+                .CommonFields());
+
+            this.CreateTableWithId64("Transaction", "Id", s => s
+                .WithColumn("TransactionType").AsInt32().NotNullable()
+                .WithColumn("RefTransactionId").AsInt64().Nullable().ForeignKey("Transaction", "Id")
+                .WithColumn("TransactionNumber").AsString(50).NotNullable()
+                .WithColumn("TransactionDate").AsDateTime().NotNullable()
+                .WithColumn("PartyId").AsInt64().Nullable().ForeignKey()
+                .CommonFields());
+
+            this.CreateTableWithId64("TransactionDetail", "Id", s => s
+                .WithColumn("TransactionId").AsInt64().NotNullable().ForeignKey()
+                .WithColumn("ProductId").AsInt64().NotNullable().ForeignKey()
+                .WithColumn("Quantity").AsDateTime().NotNullable()
+                .WithColumn("UnitPrice").AsDecimal().NotNullable()
+                .WithColumn("Amount").AsDecimal().NotNullable()
+                .CommonFields());
+
         }
     }
 }
