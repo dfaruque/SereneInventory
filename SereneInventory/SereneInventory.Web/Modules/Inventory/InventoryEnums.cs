97f27cbfdac89057ff09c53f2c8f25a218006114
dFaruque <dfaruque@gmail.com>
2018-06-02 14:23:11 +0600

    added Inventory and Setup Enums

---
+++ SereneInventory/SereneInventory.Web/Modules/Inventory/InventoryEnums.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SereneInventory
+{
+    public enum TransactionType
+    {
+        [Description("Purchase Invoice")]
+        PurchaseInvoice = 10,
+        [Description("Sales Invoice")]
+        SalesInvoice = 20,
+    }
+}
---
+++ SereneInventory/SereneInventory.Web/Modules/Setup/SetupEnums.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace SereneInventory
+{
+    public enum ProductType
+    {
+        ProductType1 = 10,
+        ProductType2 = 20,
+    }
+}
