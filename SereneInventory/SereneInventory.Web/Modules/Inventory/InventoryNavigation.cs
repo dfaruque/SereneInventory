b8b4c441ad4cde81383082318a8e9cde8ddd5cfd
dFaruque <dfaruque@gmail.com>
2018-06-02 14:14:21 +0600

    nav icon

--- SereneInventory/SereneInventory.Web/Modules/Inventory/InventoryNavigation.cs
+++ SereneInventory/SereneInventory.Web/Modules/Inventory/InventoryNavigation.cs
@@ -1,5 +1,5 @@
 ﻿using Serenity.Navigation;
 using MyPages = SereneInventory.Inventory.Pages;
 
+[assembly: NavigationMenu(9000, "Inventory", icon: "fa-line-chart")]
 [assembly: NavigationLink(int.MaxValue, "Inventory/Transaction", typeof(MyPages.TransactionController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "Inventory/Transaction Detail", typeof(MyPages.TransactionDetailController), icon: null)]
--- SereneInventory/SereneInventory.Web/Modules/Setup/SetupNavigation.cs
+++ SereneInventory/SereneInventory.Web/Modules/Setup/SetupNavigation.cs
@@ -1,6 +1,7 @@
 ﻿using Serenity.Navigation;
 using MyPages = SereneInventory.Setup.Pages;
 
+[assembly: NavigationMenu(9000, "Setup", icon: "fa-cogs")]
 [assembly: NavigationLink(int.MaxValue, "Setup/Party", typeof(MyPages.PartyController), icon: null)]
 [assembly: NavigationLink(int.MaxValue, "Setup/Product", typeof(MyPages.ProductController), icon: null)]
 [assembly: NavigationLink(int.MaxValue, "Setup/Product Category", typeof(MyPages.ProductCategoryController), icon: null)]
