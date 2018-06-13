8428588a8df5e3d0ccc74274b9744fddc1833fd9
dFaruque <dfaruque@gmail.com>
2018-06-13 18:09:52 +0600

    published

--- SereneInventory/SereneInventory.Web/App_Start/SiteInitialization.Migrations.cs
+++ SereneInventory/SereneInventory.Web/App_Start/SiteInitialization.Migrations.cs
@@ -151,12 +151,12 @@
 
             // safety check to ensure that we are not modifying an arbitrary database.
             // remove these lines if you want SereneInventory migrations to run on your DB.
-            if (!isOracle && cs.ConnectionString.IndexOf(typeof(SiteInitialization).Namespace +
-                    @"_" + databaseKey + "_v1", StringComparison.OrdinalIgnoreCase) < 0)
-            {
-                SkippedMigrations = true;
-                return;
-            }
+            ////if (!isOracle && cs.ConnectionString.IndexOf(typeof(SiteInitialization).Namespace +
+            ////        @"_" + databaseKey + "_v1", StringComparison.OrdinalIgnoreCase) < 0)
+            ////{
+            ////    SkippedMigrations = true;
+            ////    return;
+            ////}
 
             string databaseType = isOracle ? "OracleManaged" : serverType;
             var connectionString = cs.ConnectionString;
