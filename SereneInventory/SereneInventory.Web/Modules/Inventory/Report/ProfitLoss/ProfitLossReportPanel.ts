cec5861ffe431166edf86a0e9285e96a9d0028ba
D Faruque Ahmad <dfaruque@gmail.com>
2019-11-25 22:08:24 +0600

    ...

--- SereneInventory/SereneInventory.Web/Modules/Inventory/Report/ProfitLoss/ProfitLossReportPanel.ts
+++ SereneInventory/SereneInventory.Web/Modules/Inventory/Report/ProfitLoss/ProfitLossReportPanel.ts
@@ -3,7 +3,7 @@
     @Serenity.Decorators.registerClass()
     export class ProfitLossReportPanel extends _Ext.ReportPanelBase<ProfitLossReportRequest> {
         protected getReportTitle() { return 'Profit/Loss Report'; }
-        protected getReportKey() { return 'ExtraSamples.ProfitLossReport'; }
+        protected getReportKey() { return 'Inventory.ProfitLossReport'; }
         protected getFormKey() { return ProfitLossReportRequestForm.formKey; }
 
         private form: ProfitLossReportRequestForm = new ProfitLossReportRequestForm(this.idPrefix);
